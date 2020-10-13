dee412264ed518c18bd1532a893ed687c42d5039
hayashihiroaki <61njn0@gmail.com>
2020-10-13 17:17:58 +0900

    あ

--- posts/models.py
+++ posts/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
+
 
 class Post(models.Model):
-        title = models.CharField(max_length=100)
-        published = models.DateTimeField()
-        image = models.ImageField(upload_to='modia/')
-        body = models.TextField()
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
 
-        def __str__(self):
-            return self.title
+    def __str__(self):
+        return self.title
 
-        def summary(self):
-            return self.body[:30]
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
