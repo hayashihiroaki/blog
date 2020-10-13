da5fd05a1260589894fece7ce08d3a2027aa06d2
hayashihiroaki <61njn0@gmail.com>
2020-10-13 17:18:04 +0900

    Revert "あ"
    This reverts commit dee412264ed518c18bd1532a893ed687c42d5039.

--- posts/models.py
+++ posts/models.py
@@ -1,17 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.utils import timezone
-from django.urls import reverse
-
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_posted = models.DateTimeField(default=timezone.now)
+        title = models.CharField(max_length=100)
+        published = models.DateTimeField()
+        image = models.ImageField(upload_to='modia/')
+        body = models.TextField()
 
-    def __str__(self):
-        return self.title
+        def __str__(self):
+            return self.title
 
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
+        def summary(self):
+            return self.body[:30]
